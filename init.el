d2cf8daf9abbc16c5f20d7f2078933212e998b35
abicky <takeshi.arabiki@gmail.com>
2014-01-02 03:32:14 +0900

    Add all directories in 'vendor' to load-path

--- init.el
+++ init.el
@@ -3,9 +3,13 @@
   ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
   (package-initialize))
 
-(when (and (not noninteractive)  ; not executed with --script option?
-           (require 'init-loader nil t))
-  (setq init-loader-byte-compile t
-        init-loader-show-log-after-init nil
-        init-loader-directory (expand-file-name "config" user-emacs-directory))
-  (init-loader-load))
+(when (not noninteractive)  ; not executed with --script option?
+  (when (require 'init-loader nil t)
+    (setq init-loader-byte-compile t
+          init-loader-show-log-after-init nil
+          init-loader-directory (expand-file-name "config" user-emacs-directory))
+    (init-loader-load))
+  ;; Add load paths
+  (let ((default-directory (expand-file-name "~/.emacs.d/vendor")))
+    (add-to-list 'load-path default-directory)
+    (normal-top-level-add-subdirs-to-load-path)))
