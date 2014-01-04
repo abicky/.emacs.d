9264a7e04ff998edcc9405cc8a59bfb4fb9cb4c0
abicky <takeshi.arabiki@gmail.com>
2014-01-05 03:01:14 +0900

    Customize `helm-find-files' and `helm-buffers-list'

--- config/10-helm.el
+++ config/10-helm.el
@@ -1,5 +1,5 @@
 (when (require 'helm-config nil t)
-  (helm-mode t)
+  (helm-mode 1)
   ;; keybindings for global-map are defined in keybinding.el
   (define-key helm-map (kbd "C-h") 'delete-backward-char)
   (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
@@ -13,7 +13,43 @@
 
   ;; Emulate `kill-line' in helm minibuffer
   (setq helm-delete-minibuffer-contents-from-point t)
-  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+  (defadvice helm-delete-minibuffer-contents (before emulate-kill-line activate)
     "Emulate `kill-line' in helm minibuffer"
     (kill-new (buffer-substring (point) (field-end))))
+
+  (defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-file-exist activate)
+    "Execute command only if CANDIDATE exists"
+    (when (file-exists-p candidate)
+      ad-do-it))
+
+  (setq helm-ff-smart-completion nil)
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+           (input-pattern (file-name-nondirectory pattern))
+           (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+            (concat dirname
+                    (if (string-match "^\\^" input-pattern)
+                        ;; '^' is a pattern for basename
+                        ;; and not required because the directory name is prepended
+                        (substring input-pattern 1)
+                      (concat ".*" input-pattern))))))
+
+  (defun helm-buffers-list-pattern-transformer (pattern)
+    (if (equal pattern "")
+        pattern
+      ;; Escape '.' to match '.' instead of an arbitrary character
+      (setq pattern (replace-regexp-in-string "\\." "\\\\." pattern))
+      (let ((first-char (substring pattern 0 1)))
+        (cond ((equal first-char "*")
+               (concat " " pattern))
+              ((equal first-char "=")
+               (concat "*" (substring pattern 1)))
+              (t
+               pattern)))))
+
+  (add-to-list 'helm-source-buffers-list
+               '(pattern-transformer helm-buffers-list-pattern-transformer))
   )
