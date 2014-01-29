4339d6d86656c929c3342ee2f244fc2cf6820653
abicky <takeshi.arabiki@gmail.com>
2014-01-30 00:57:24 +0900

    Make `electric-indent-chars' a buffer local variable to enable `electric-indent-mode' only in specific major modes

--- config/10-electric.el
+++ config/10-electric.el
@@ -1,6 +1,12 @@
-(eval-after-load "electric"
-  '(defadvice electric-pair-post-self-insert-function
-     (around electric-pair-post-self-insert-function-around activate)
-     "Don't insert the closing pair in comments or strings"
-     (unless (nth 8 (save-excursion (syntax-ppss (1- (point)))))
-       ad-do-it)))
+(defun electric-after-load ()
+  ;; Make `electric-indent-chars' a buffer local variable
+  ;; to enable `electric-indent-mode' only in specific major modes
+  (set-default (make-variable-buffer-local 'electric-indent-chars) nil)
+
+  (defadvice electric-pair-post-self-insert-function
+    (around electric-pair-post-self-insert-function-around activate)
+    "Don't insert the closing pair in comments or strings"
+    (unless (nth 8 (save-excursion (syntax-ppss (1- (point)))))
+      ad-do-it))
+  )
+(eval-after-load "electric" 'electric-after-load)
